7f71045e0712a41bb9f0d771e2fadb6984a11f81
Jiří Vrba <jiri@vrba.dev>
2021-02-09 14:23:28 +0100

    Initial commit

---
+++ src/main/java/cz/vse/fis/minecraft/naturalselection/NaturalSelection.java
@@ -0,0 +1,15 @@
+package cz.vse.fis.minecraft.naturalselection;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NaturalSelection extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // yee yee ass logic here
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
